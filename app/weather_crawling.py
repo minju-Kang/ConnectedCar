fa0e7d8050525ec35761fc1c03ba2fbc0c80b2a5
Smineee11 <smin3407ch@gmail.com>
2021-05-28 18:25:15 +0900

    Modify weather widget design

--- app/weather_crawling.py
+++ app/weather_crawling.py
@@ -27,19 +27,79 @@ def weather():
 
     data3 = soup.find('div', {'class': 'main_info'})
     weather = data3.find('p',{'class': 'cast_txt'}).text
-  
+    cnt = 0
+    for ch in weather:
+        cnt = cnt+1
+        if ch == "어":
+            img_text = weather[0:cnt-3]
+    print(img_text) #이미지를 가져오기 위한 텍스트 추출
+    img_src=""
+    if img_text == "맑음" or img_text == "구름조금":
+        print("날씨", img_text)
+        img_src = "https://image.flaticon.com/icons/png/128/869/869869.png"
+    elif img_text == "구름많음" or img_text == "흐림":
+        img_src = "https://image.flaticon.com/icons/png/128/494/494435.png"
+    elif img_text == "약한비" or img_text =="비" or img_text =="강한비" or img_text =="소나기":
+        img_src = "https://image.flaticon.com/icons/png/128/3313/3313966.png"
+    elif img_text == "약한눈" or img_text =="눈" or img_text =="강한눈" or img_text =="진눈깨비" or img_text =="소낙눈":
+        img_src = "https://image.flaticon.com/icons/png/128/3026/3026312.png"
+    else:
+        img_src = "https://image.flaticon.com/icons/png/128/869/869869.png"
+
 
     data2 = data1.findAll('dd')
     dust = data2[0].find('span', {'class':'num'}).text
     ultra_dust = data2[1].find('span', {'class':'num'}).text
-    ozone = data2[2].find('span', {'class':'num'}).text
+    cnt =0
+    for ch in dust:
+        cnt = cnt+1
+        if ch == "㎍":
+            dust_num = dust[0:cnt-1]
+    cnt=0
+    for ch in ultra_dust:
+        cnt = cnt+1
+        if ch == "㎍":
+            ultra_dust_num = ultra_dust[0:cnt-1]
+    print(ultra_dust_num,dust_num)
+    dust_state = ""
+    ultra_dust_state = ""
+    dust_src = ""
+    ultra_dust_src = ""
+    if int(dust_num) >=0 and int(dust_num)<=30:
+        dust_state = "좋음"
+        dust_src = "https://image.flaticon.com/icons/png/128/725/725107.png"
+    elif int(dust_num) > 30 and int(dust_num) <=80:
+        dust_state = "보통"
+        dust_src = "https://image.flaticon.com/icons/png/128/725/725105.png"
+    elif int(dust_num) > 80 and int(dust_num) <=150:
+        dust_state = "나쁨"
+        dust_src = "https://image.flaticon.com/icons/png/128/725/725085.png"
+    elif int(dust_num) <150:
+        dust_state = "매우나쁨"
+        dust_src = "https://image.flaticon.com/icons/png/128/725/725099.png"
+    
+    if int(ultra_dust_num) >=0 and int(ultra_dust_num)<=15:
+        ultra_dust_state = "좋음"
+        ultra_dust_src = "https://image.flaticon.com/icons/png/128/725/725107.png"
+    elif int(ultra_dust_num) > 15 and int(ultra_dust_num) <=35:
+        ultra_dust_state = "보통"
+        ultra_dust_src = "https://image.flaticon.com/icons/png/128/725/725105.png"
+    elif int(ultra_dust_num) > 35 and int(ultra_dust_num) <=75:
+        ultra_dust_state = "나쁨"
+        ultra_dust_src = "https://image.flaticon.com/icons/png/128/725/725085.png"
+    elif int(ultra_dust_num) <75:
+        ultra_dust_state = "매우나쁨"
+        ultra_dust_src = "https://image.flaticon.com/icons/png/128/725/725099.png"
 
     context_new = {
         'address' : address,
         'temp': currenttemp,
         'weather': weather,
-        'dust': dust,
-        'ultra_dust': ultra_dust
+        'dust': dust_state,
+        'ultra_dust': ultra_dust_state,
+        'img_src' : img_src,
+        'dust_src': dust_src,
+        'ultra_dust_src' : ultra_dust_src
     }
 
     return context_new
